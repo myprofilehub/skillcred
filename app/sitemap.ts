da2ab42adcb4ec4a264b6372c010da1268d42cdd
myprofilehub <mgmags25@gmail.com>
2026-03-19 03:54:45 +0530

    sitemap updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "https://skillcred.com";
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || process.env.AUTH_URL || "https://www.skillcred.in";
 
   // Public primary routes
   const routes = [
